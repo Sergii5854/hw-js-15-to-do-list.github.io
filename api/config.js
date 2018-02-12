f0a42684cc3e2de9fa7c275220657af3221a61d9
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-02-12 13:30:15 +0200

    get conection from server to db and from front to db

--- api/config.js
+++ api/config.js
@@ -1,6 +1,17 @@
 const host = 'localhost'
+// backend
+var dbuser = 'Sergii5854';
+var dbpassword = 'nafalin132435';
+var dbURL = `mongodb://${dbuser}:${dbpassword}@ds127888.mlab.com:27888/todo-list-js7`;
+
+
+// frontend
+const APIKEY= '?apiKey=ub5BFaJp0V2ASPxrYFiNY-ACVx-EEtAG';
+const DBURL_todo = `https://api.mlab.com/api/1/databases/todo-list-js7/collections/todo${APIKEY}`;
+const DBURL_completed = `https://api.mlab.com/api/1/databases/todo-list-js7/collections/completed${APIKEY}`;
+
 
 module.exports = {
   port: process.env.port || process.env.PORT || '3000',
-  mongoUrl: process.env.MONGODB_URI || `mongodb://${host}/foods`
-}
+  mongoUrl: process.env.MONGODB_URI || dbURL
+};
--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const app = express()
 const config = require('./api/config')
 app.listen(config.port, function()  {
   console.log(`Server running at port: ${config.port}`)
-})
+});
 
 // added for development
 app.get('/favicon.ico', function(req, res) {
   res.status(204)
-})
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'index.html'))
-})
+});
 
 app.use(bodyParser.json())
 
@@ -26,20 +26,22 @@ app.use(function(req, res, next) {
   const err = new Error(`Not Found ${req.path}`)
   err.status = 404
   next(err)
-})
+});
+
 app.use(function(error, req, res, next) {
   if (error) {
     console.log(error)
     return res.status(400).json({error})
   }
   next(error)
-})
+});
+
 app.use(function(err, req, res, next)  {
   res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
   })
-})
+});
 
 module.exports = app
